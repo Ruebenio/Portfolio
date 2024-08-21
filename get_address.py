e15a8ff3b5ea69f2e5459d6510f6521f341adeb7
Nana Yaw Adjei <a00293983@mycambrian.ca>
2024-08-21 04:41:00 +0000

    Masked secrets

--- get_address.py
+++ get_address.py
@@ -1,11 +1,12 @@
 # importing librairies
 import urllib.request
 import json
+import os
 
 # defining the address function using the reverse geolocation API
 def address(lat,lon):
-  key='bdc_5a5c1ca777334342b5e596fa84aa1784'
-  url=f'https://api-bdc.net/data/reverse-geocode?latitude={lat}&longitude={lon}&localityLanguage=en&key={key}'
+  gkey = os.environ['AD_KEY']
+  url=f'https://api-bdc.net/data/reverse-geocode?latitude={lat}&longitude={lon}&localityLanguage=en&key={gkey}'
   request = urllib.request.urlopen(url)
   result = json.loads(request.read())
   return result
